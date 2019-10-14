a9ef88c2d843fe772fcf514aca90a6212f00cdbc
Nils Schaetti <nils.schaetti@unine.ch>
2019-10-14 15:04:10 +0200

    Measures sub-package

---
+++ measures/__init__.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+#
+# File : __init__.py
+# Description : Measure sub-package
+# Date : 14th of October, 2019
+#
+# This file is part of the Conceptor package.  The Conceptor package is free
+# software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+#
+
+
+# Imports
+from .nrsme import nrmse, nrmse_aligned
+
+# All
+__all__ = ['nrmse', 'nrmse_aligned']
