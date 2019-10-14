16960d008052aec5c7daef44e2b82cab68fb47cc
Nils Schaetti <nils.schaetti@unine.ch>
2019-10-14 15:03:34 +0200

    Conceptor Logic sub-package

---
+++ logic/__init__.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+#
+# File : and.py
+# Description : AND in Conceptor Logic
+# Date : 14th of October, 2019
+#
+# This file is part of the Conceptor package.  The Conceptor package is free
+# software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+#
+
+
+# Imports
+from .and_operator import AND
+from .not_operator import NOT
+from .or_operator import OR
+from .phi_operator import PHI
+
+
+# ALL
+__all__ = ['AND', 'NOT', 'OR', 'PHI']
