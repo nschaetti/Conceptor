6db6dc69963fc7d243822ca981d57a8cc8d0ae33
schaetti <snow2458-->
2019-10-14 18:24:14 +0200

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Conceptors',
+    version='0.1',
+    packages=['logic', 'measures', 'patterns', 'reservoir', 'conceptors', 'visualization'],
+    url='https://github.com/nschaetti/Conceptor',
+    license='GPL version 3',
+    author='Nils Schaetti',
+    author_email='n.schaetti@gmail.com',
+    description='Tools and functions to explore conceptors and reservoir loading methods'
+)
