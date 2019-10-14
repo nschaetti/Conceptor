7915fc75074d6b21ecfe6131b4d9e194c0ebab42
schaetti <snow2458-->
2019-10-14 19:27:07 +0200

    Test bases

---
+++ tests/test_free_run.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+#
+# File : test_free_run.py
+# Description : Test functions to generate patterns with conceptors.
+# Date : 14th of October, 2019
+#
+# This file is part of the Conceptor package.  The Conceptor package is free
+# software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+#
+
+# Imports
+from unittest import TestCase
+
+
+# Test reservoir free run and generation
+class Test_Free_Run(TestCase):
+
+    # Test free run
+    def test_free_run(self):
+        """
+        Test free run
+        :return:
+        """
+        self.fail()
+    # end test_free_run
+
+    # Test free run with input simulation
+    def test_free_run_input_simulation(self):
+        """
+        Test free run with input simulation
+        :return:
+        """
+        self.fail()
+    # end test_incremental_loading
+
+    # Test incremental training
+    def test_incremental_training(self):
+        """
+        Test incremental training
+        :return:
+        """
+        self.fail()
+    # end test_incremental_training
+
+# end Test_Loading
---
+++ tests/test_loading.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+#
+# File : test_loading.py
+# Description : Test functions for loading reservoirs.
+# Date : 14th of October, 2019
+#
+# This file is part of the Conceptor package.  The Conceptor package is free
+# software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+#
+
+# Imports
+from unittest import TestCase
+
+
+# Test reservoir loading
+class Test_Loading(TestCase):
+
+    # Test loading
+    def test_loading(self):
+        """
+        Test loading
+        :return:
+        """
+        self.fail()
+    # end test_loading
+
+    # Test incremental loading
+    def test_incremental_loading(self):
+        """
+        Test incremental loading
+        :return:
+        """
+        self.fail()
+    # end test_incremental_loading
+
+    # Test incremental training
+    def test_incremental_training(self):
+        """
+        Test incremental training
+        :return:
+        """
+        self.fail()
+    # end test_incremental_training
+
+# end Test_Loading
